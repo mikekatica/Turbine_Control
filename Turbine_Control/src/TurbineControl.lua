9f0d933a1f7f93bf7e5b13b144721b12784b2073
Mike <michael.katica@gmail.com>
2015-03-15 13:00:34 -0400

    current params display on menu

--- Turbine_Control/src/TurbineControl.lua
+++ Turbine_Control/src/TurbineControl.lua
@@ -3,7 +3,7 @@ rstTerm = function ()
   term.clear()
   term.setCursorPos(1,1)
 end
-printMenu = function (t)
+printMenu = function (c)
   row = 1
   term.setCursorPos(1,row)
   row = row + 1
@@ -29,6 +29,9 @@ printMenu = function (t)
   row = row + 1
   term.setBackgroundColor(colors.black)
   term.write("  ")
+  term.setCursorPos(1,row)
+  row = row + 1
+  term.write("Target:  "..c.target.."  Kp: "..c.kp.."  Ki: "..c.ki.."  Kd: "..c.kd)
 end
 updateDisp = function (c)
   while true do
@@ -41,7 +44,7 @@ run = function (c)
   term.clear()
   while not exit do
     rstTerm()
-    printMenu()
+    printMenu(c)
     local event, button, xPos, yPos = os.pullEvent("mouse_click")
     if event then
       if yPos == 1 then
@@ -81,7 +84,7 @@ m = peripheral.wrap("top")
 l = tctl.logger
 l.setFilename(l, nil)
 l.createFile(l)
-c.setUp(c,t,m,800,l)
+c.setUp(c,t,m,2000,l)
 c.setTarget(c,1800)
 parallel.waitForAny(function() run(c) end, function() updateDisp(c) end)
 print("Complete")

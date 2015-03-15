102f8a951c5dc53e1c70f2449d593b4e507aaf0c
Mike <michael.katica@gmail.com>
2015-03-15 11:08:31 -0400

    added coroutine helper for diaply in TurbineControl

--- Turbine_Control/src/TurbineControl.lua
+++ Turbine_Control/src/TurbineControl.lua
@@ -2,18 +2,24 @@ if not tctl then
   os.loadAPI("soft/tctl")
 end
 rstTerm = function ()
-            term.clear()
-            term.setCursorPos(1,1)
-          end
+  term.clear()
+  term.setCursorPos(1,1)
+end
 printMenu = function (t)
-            row = 1
-            term.setCursorPos(1,row)
-            row = row + 1
-            term.setBackgroundColor(colors.red)
-            term.write("Activate Turbine")
-            term.setCursorPos(1,row)
-            term.setBackgroundColoe(colors.black)
-            end
+  row = 1
+  term.setCursorPos(1,row)
+  row = row + 1
+  term.setBackgroundColor(colors.red)
+  term.write("Activate Turbine")
+  term.setCursorPos(1,row)
+  term.setBackgroundColoe(colors.black)
+end
+updateDisp = function (c)
+  while true do
+    c.updateDisplay(c)
+    sleep(0.1)
+  end
+end
 c = tctl.control
 t = peripheral.wrap("back")
 m = peripheral.wrap("top")
@@ -23,20 +29,19 @@ l.setFilename(l, nil)
 l.createFile(l)
 c.setUp(c,t,m,800,l)
 term.clear()
-d = multisehll.launch({},"soft/DisplayControl")
-multishell.setTitle(d, "Display")
+
 while not exit do
-local event, button, xPos, yPos = os.pullEvent("mouse_click")
-rstTerm()
-printMenu()
-c.updateDisplay(c, nil)
-if event then
-  if yPos < 2 and yPos > 0 then
-    rstTerm()
-    print("Turbine Spinning Up")
-    c.setTarget(c, 1800)
-    c.begin(c)
+  local event, button, xPos, yPos = os.pullEvent("mouse_click")
+  rstTerm()
+  printMenu()
+  c.updateDisplay(c, nil)
+  if event then
+    if yPos < 2 and yPos > 0 then
+      rstTerm()
+      print("Turbine Spinning Up")
+      c.setTarget(c, 1800)
+      c.begin(c)
+    end
   end
-end
-sleep(0.5)
+  sleep(0.5)
 end

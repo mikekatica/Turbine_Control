d4131355dfe553a4a3e70152a8a956be3eb3be09
Mike <michael.katica@gmail.com>
2015-03-15 11:24:21 -0400

    coroutine to parallel

--- Turbine_Control/src/TurbineControl.lua
+++ Turbine_Control/src/TurbineControl.lua
@@ -1,6 +1,11 @@
 if not tctl then
   os.loadAPI("soft/tctl")
 end
+  c = tctl.control
+  t = peripheral.wrap("back")
+  m = peripheral.wrap("top")
+  l = tctl.logger
+  c.setUp(c,t,m,800,l)
 rstTerm = function ()
   term.clear()
   term.setCursorPos(1,1)
@@ -14,34 +19,30 @@ printMenu = function (t)
   term.setCursorPos(1,row)
   term.setBackgroundColoe(colors.black)
 end
-updateDisp = function (c)
+updateDisp = function ()
   while true do
     c.updateDisplay(c)
     sleep(0.1)
   end
 end
-c = tctl.control
-t = peripheral.wrap("back")
-m = peripheral.wrap("top")
-l = tctl.logger
-exit = false
-l.setFilename(l, nil)
-l.createFile(l)
-c.setUp(c,t,m,800,l)
-term.clear()
-local c1 = coroutine.create(updateDisp)
-coroutine.resume(c1, c)
-while not exit do
-  rstTerm()
-  printMenu()
-  local event, button, xPos, yPos = os.pullEvent("mouse_click")
-  if event then
-    if yPos < 2 and yPos > 0 then
-      rstTerm()
-      print("Turbine Spinning Up")
-      c.setTarget(c, 1800)
-      c.begin(c)
+run = function ()
+  exit = false
+  l.setFilename(l, nil)
+  l.createFile(l)
+  term.clear()
+  while not exit do
+    rstTerm()
+    printMenu()
+    local event, button, xPos, yPos = os.pullEvent("mouse_click")
+    if event then
+      if yPos < 2 and yPos > 0 then
+        rstTerm()
+        print("Turbine Spinning Up")
+        c.setTarget(c, 1800)
+        c.begin(c)
+      end
     end
+    sleep(0.5)
   end
-  sleep(0.5)
 end
+parallel.waitForAny(run, updateDisp)
